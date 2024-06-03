70a185d3fd2ab4fa4d2005e6c4cbcd7b789f709a
ayadag <109915071+ayadag@users.noreply.github.com>
2024-06-03 18:35:54 +0300

    RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,55 @@
-import './globals.css';
+// import { NextPage } from "next";
+// import { AppProps } from "next/app";
+// import { FC } from "react";
+import { ToastContainer } from "react-toastify";
+import { SerumProvider } from "../context/SerumContext";
+import { SolanaProvider } from "../context/SolanaContext";
 
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Lama Dev Next.js Admin Dashboard',
-  description: 'Next.js Tutorial',
-}
+// Use require instead of import since order matters
+require("@solana/wallet-adapter-react-ui/styles.css");
+require("../styles/globals.css");
+require("react-toastify/dist/ReactToastify.css");
 
-export default function RootLayout({ children }) {
-  return (
+// type NextPageWithLayout = NextPage & {
+//   getLayout?: (page: React.ReactNode) => React.ReactNode;
+// };
+
+// type AppPropsWithLayout = AppProps & {
+//   Component: NextPageWithLayout;
+// };
+
+export default function RootLayout({children}){
+  return(
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <SolanaProvider>
+        <SerumProvider>
+          <ToastContainer theme="dark" position="bottom-right" autoClose={2000} />
+          <body className={inter.className}>{children}</body>
+        </SerumProvider>
+      </SolanaProvider>
     </html>
-  )
-}
+  );
+};
+
+// import './globals.css';
+
+// import { Inter } from 'next/font/google';
+
+// const inter = Inter({ subsets: ['latin'] })
+
+// export const metadata = {
+//   title: 'Lama Dev Next.js Admin Dashboard',
+//   description: 'Next.js Tutorial',
+// }
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body className={inter.className}>{children}</body>
+//     </html>
+//   )
+// }
